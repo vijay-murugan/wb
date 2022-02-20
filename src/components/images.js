a134989f59b686fe4aea632456d1a57d1db8f922
vijay-murugan <56683057+vijay-murugan@users.noreply.github.com>
2022-02-20 12:04:37 +0530

    ninth

--- src/components/images.js
+++ src/components/images.js
@@ -8,7 +8,7 @@ const Images = () => {
   const [data,setData]=useState([]);
   const display = () => {
 
-  axios.get(`http://localhost:5000/images/${id}`).then((response)=> {         
+  axios.get(`https://wba-a.herokuapp.com/images/${id}`).then((response)=> {         
   setData(response)
 });
   }
